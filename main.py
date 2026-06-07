62f98734aa3871bdc5f103ff392a15dfd5d7132c
Jeet Kundu <kundujeet916@gmail.com>
2026-06-08 00:31:10 +0530

    Student Management System using Python and MySQL

--- main.py
+++ main.py
@@ -3,12 +3,11 @@ import mysql.connector
 conn = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="-----",
+    password="Root@123",
     database="student_db"
 )
 
 cursor = conn.cursor()
-
 
 def add_student():
     name = input("Enter Name: ")
